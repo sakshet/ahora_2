dfd814ecf2884d3d98a68c30a952451e34c07c00
Sakshet <sakshet@outlook.com>
2022-04-08 23:03:11 +0100

    Bundle

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+<<<<<<< HEAD
 import { HomeContainer } from './containers';
+=======
+import { HomeContainer } from '../src/containers';
+>>>>>>> 31822e3 (Bundle)
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
